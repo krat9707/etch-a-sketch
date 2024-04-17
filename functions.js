65df9fc1090edab18fa95f6fde9178ca85feadf3
krat <kashyapbaraiya9707@gmail.com>
2024-04-17 10:27:41 +0530

    Add trail Effect on mouse hover

--- functions.js
+++ functions.js
@@ -1,8 +1,9 @@
 let gridSize = document.querySelector("#gridSizeSlider");
 gridSize.addEventListener('input', makeGrid);
+
 makeGrid();
 
-function makeGrid()
+function makeGrid() 
 {
     let gridContainer = document.querySelector("#grid");
     gridContainer.innerHTML = '';
@@ -18,4 +19,30 @@ function makeGrid()
             gridContainer.appendChild(gridCell);
         }
     }
+
+    setupEventListener();
+}
+
+function setupEventListener()
+{
+    let clickInsideGrid = false;
+    let gridCell = document.querySelectorAll(".gridCell");
+    let grid = document.querySelector("#grid");    
+
+    grid.addEventListener('mousedown', function (event) {
+        clickInsideGrid = true;
+    });
+
+    grid.addEventListener('mouseup', function(event) {
+        clickInsideGrid = false;
+    });
+
+    gridCell.forEach(cell => {
+        cell.addEventListener('mousemove', function(event) {
+            console.log('mousemove event, clickInsideGrid:', clickInsideGrid);
+        
+            if(clickInsideGrid === true)
+                cell.style.backgroundColor = "black";
+        });    
+    });
 }
