6716d5d0712904318174fcee977ff597221b0bd1
krat <kashyapbaraiya9707@gmail.com>
2024-04-19 16:25:33 +0530

    Add burn and dodge

--- functions.js
+++ functions.js
@@ -28,13 +28,31 @@ function handleButtonClick(buttonName) {
     });
 
     const currentButton = buttons.get(buttonName);
-    window[buttonName] = !window[buttonName];
-    console.log(buttonName);
-    currentButton.classList.toggle('activeBtn');
+
+    if(currentButton)
+    {
+        window[buttonName] = !window[buttonName];
+        currentButton.classList.toggle('activeBtn');
+    }
 } 
 
+let prevColorBeforeEraser = "#EDC7B7";
 gridSize.addEventListener('input', makeGrid);
-eraser.addEventListener('click', () => {handleButtonClick('eraserBool'), penColor.value = "#EDC7B7"});
+
+penColor.addEventListener('click', () => handleButtonClick('penColor'));
+eraser.addEventListener('click', () => {
+    if(eraserBool === false)
+    {
+        prevColorBeforeEraser = penColor.value;
+        penColor.value = "#EDC7B7";
+    }
+    else
+    {   
+        penColor.value = prevColorBeforeEraser;
+    }   
+    
+    handleButtonClick('eraserBool');
+});
 rainbow.addEventListener('click', () => handleButtonClick('rainbowPen'));
 burnBtn.addEventListener('click', () => handleButtonClick('burn'));
 dodgeBtn.addEventListener('click', () => handleButtonClick('dodge'));
@@ -46,7 +64,7 @@ gridsVisibleBtn.addEventListener('click', () => {
     {
         visibleGrid = true;
         gridsVisibleBtn.innerHTML = '&nbsp;&nbsp;Hide Grids';
-        gridCell.forEach(cell => cell.style.border = "0.1px solid black"); 
+        gridCell.forEach(cell => cell.style.border = "1px solid black"); 
     }
     else
     {
@@ -72,10 +90,11 @@ function makeGrid()
             let gridCell = document.createElement("div");
             gridCell.classList.add("gridCell");
             gridCell.style.cssText = `width: ${100/gridSize.value}%; height: ${100/gridSize.value}%`; 
+            gridCell.style.backgroundColor = '#EDC7B7';
 
             if(visibleGrid === true)
-                gridCell.style.border = "0.1px solid black";
-
+                gridCell.style.border = "1px solid black";
+            
             gridContainer.appendChild(gridCell);
         }
     }
@@ -98,14 +117,17 @@ function setupEventListener()
     });
 
     gridCell.forEach(cell => {
-        cell.addEventListener('mousemove', function(event) {
+        cell.addEventListener('mouseenter', function(event) {
         
             if(clickInsideGrid === true)
             {
                 if(dodge === false && burn === false)
                 {
                     if(rainbowPen === false)
+                    {
                         cell.style.backgroundColor = penColor.value;
+                        cell.removeAttribute('data-shade');
+                    }
                     else
                     {
                         let r = getRandomColor();
@@ -117,7 +139,17 @@ function setupEventListener()
                 }
                 else
                 {
-                    
+                    if(!cell.hasAttribute('data-shade'))
+                    {
+                        cell.setAttribute('data-shade', '0');
+                    }
+                    let shade = parseInt(cell.getAttribute('data-shade'));
+
+                    if(dodge === true)  shade++;       
+                    else    shade--;
+
+                    cell.setAttribute('data-shade', `${shade}`);
+                    cell.style.backgroundColor = adjust(rgbToHex, cell.style.backgroundColor, shade * -2);
                 }
             }
         });    
@@ -131,3 +163,40 @@ function getRandomColor()
 {
     return (Math.floor(Math.random() * 256));
 }
+
+function rgbToHex(rgb) {
+    let sep = rgb.indexOf(',') > -1 ? ',' : ' ';
+
+    rgb = rgb.substr(4).split(')')[0].split(sep);
+    
+    let r = (+rgb[0]).toString(16),
+        g = (+rgb[1]).toString(16),
+        b = (+rgb[2]).toString(16);
+    
+    if (r.length == 1) r = '0' + r;
+    if (g.length == 1) g = '0' + g;
+    if (b.length == 1) b = '0' + b;
+    b = (+rgb[2]).toString(16);
+    
+    if (r.length == 1) r = '0' + r;
+    if (g.length == 1) g = '0' + g;
+    if (b.length == 1) b = '0' + b;
+    return '#' + r + g + b;
+}
+
+function adjust(rgbToHex, rgb, amount) 
+{
+    let color = rgbToHex(rgb);
+    return (
+      '#' +
+      color
+        .replace(/^#/, '')
+        .replace(/../g, (color) =>
+          ('0' + Math.min(255, Math.max(0, parseInt(color, 16) + amount)).toString(16)).substr(-2)
+        )
+    );
+}
+  
+  
+
+  
