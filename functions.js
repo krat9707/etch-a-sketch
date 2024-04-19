6a63ac4b56cbb7d2db1ef8f45706e87ba78540a3
krat <kashyapbaraiya9707@gmail.com>
2024-04-19 17:21:27 +0530

    Add comments

--- functions.js
+++ functions.js
@@ -1,3 +1,4 @@
+// assigning DOM elements 
 let gridSize = document.querySelector("#gridSizeSlider");
 let penColor = document.querySelector("#penColor");
 let eraser = document.querySelector("#eraser");
@@ -6,12 +7,18 @@ let rainbow = document.querySelector("#rainbowPenColor");
 let dodgeBtn = document.querySelector("#dodge");
 let burnBtn = document.querySelector("#burn");
 
+
+
+//declaring booleans for button status
 window.visibleGrid = false;
 window.rainbowPen = false;
 window.burn = false;
 window.dodge = false;
 window.eraserBool = false;
 
+
+
+//declaring Map data structure to keep all the buttons and DOMelements of buttons in pair
 const buttons = new Map([
     ['rainbowPen', rainbow],
     ['burn', burnBtn],
@@ -19,6 +26,9 @@ const buttons = new Map([
     ['eraserBool', eraser],
 ]);
 
+
+
+//takes the button clicked as parameter and iterates through whole map and turns off all other buttons except the one click and toggles the clicked btn.
 function handleButtonClick(buttonName) {
     buttons.forEach((buttonElement, buttonKey) => {
         if (buttonKey !== buttonName) {
@@ -36,10 +46,13 @@ function handleButtonClick(buttonName) {
     }
 } 
 
-let prevColorBeforeEraser = "#EDC7B7";
-gridSize.addEventListener('input', makeGrid);
 
-penColor.addEventListener('click', () => handleButtonClick('penColor'));
+
+gridSize.addEventListener('input', makeGrid);
+//saves the color that user selected(default to bgColor) 
+
+let prevColorBeforeEraser = "#EDC7B7";
+//saves the current color used by user while using eraser and assigns back once turned off
 eraser.addEventListener('click', () => {
     if(eraserBool === false)
     {
@@ -53,10 +66,10 @@ eraser.addEventListener('click', () => {
     
     handleButtonClick('eraserBool');
 });
+penColor.addEventListener('click', () => handleButtonClick('penColor'));
 rainbow.addEventListener('click', () => handleButtonClick('rainbowPen'));
 burnBtn.addEventListener('click', () => handleButtonClick('burn'));
 dodgeBtn.addEventListener('click', () => handleButtonClick('dodge'));
-
 gridsVisibleBtn.addEventListener('click', () => {
     let gridCell = document.querySelectorAll(".gridCell");
 
@@ -76,6 +89,8 @@ gridsVisibleBtn.addEventListener('click', () => {
 
 makeGrid();
 
+
+//clears off the container first and then start nested loop for the selected grid size
 function makeGrid() 
 {
     let gridContainer = document.querySelector("#grid");
@@ -87,27 +102,31 @@ function makeGrid()
     {
         for(let j = 0; j < gridSize.value; ++j)
         {
+            //creates the element in memory first and class is assigned for gridCell css styles so that it's style can be manipulated directly 
             let gridCell = document.createElement("div");
             gridCell.classList.add("gridCell");
-            gridCell.style.cssText = `width: ${100/gridSize.value}%; height: ${100/gridSize.value}%`; 
-            gridCell.style.backgroundColor = '#EDC7B7';
-
-            if(visibleGrid === true)
+            gridCell.style.cssText = `width: ${100/gridSize.value}%; height: ${100/gridSize.value}%`; //with respect to selected size of grid, size of gridCell is calculated 
+            gridCell.style.backgroundColor = '#EDC7B7'; //default color of background is selected. This color is preferred for theme of the website
+                
+            if(visibleGrid === true)    // if the grid button was set ON and size is changed, then we keep grids upon the creation of new Grid
                 gridCell.style.border = "1px solid black";
             
-            gridContainer.appendChild(gridCell);
+            gridContainer.appendChild(gridCell); // finally appending to the DOM tree
         }
     }
 
-    setupEventListener();
+    setupEventListener(); // calling this function to setup the eventListeners on newly created Cells and grid
 }
 
 function setupEventListener()
 {
+    //Boolean for tracking if click was made inside grid
     let clickInsideGrid = false;
+    //DOMelements declared, gridCell is an Array of gridCells
     let gridCell = document.querySelectorAll(".gridCell");
     let grid = document.querySelector("#grid");    
 
+    // below two eventListeners together achieves the click & drag logic
     grid.addEventListener('mousedown', function (event) {
         clickInsideGrid = true;
     });
@@ -116,54 +135,65 @@ function setupEventListener()
         clickInsideGrid = false;
     });
 
+    // iterating through all the gridCell and assinging them eventListener
     gridCell.forEach(cell => {
         cell.addEventListener('mouseenter', function(event) {
-        
+            
+            // if there was click inside grid and is being dragged over this cell then --
             if(clickInsideGrid === true)
             {
+                // if dodge & burn wasnt the one clicked then we have to assign 100% opace color (let it either be random color or user preferred)
                 if(dodge === false && burn === false)
                 {
                     if(rainbowPen === false)
                     {
                         cell.style.backgroundColor = penColor.value;
-                        cell.removeAttribute('data-shade');
-                    }
+                        cell.removeAttribute('data-shade'); // any previous shading done to color will be removed upon painting the cell with new color
+                    }   
                     else
                     {
+                        // get three random values from 0 <-> 255
                         let r = getRandomColor();
                         let g = getRandomColor();
                         let b = getRandomColor();
 
+                        // assigning these three values to the cellColor
                         cell.style.backgroundColor = "rgb("+r+", "+g+", "+b+")";
                     }
                 }
                 else
                 {
+                    // if there was no shading done before we start off with assigning it with data-shade attribute set to 0
                     if(!cell.hasAttribute('data-shade'))
                     {
                         cell.setAttribute('data-shade', '0');
                     }
-                    let shade = parseInt(cell.getAttribute('data-shade'));
+                    let shade = parseInt(cell.getAttribute('data-shade'));  // we access the data-shade current value of the cell
 
+                    // we increase the shade or decrease depending on what button was clicked
                     if(dodge === true)  shade++;       
                     else    shade--;
 
+                    //assigning new shade value to the cell so to maintain the shade value
                     cell.setAttribute('data-shade', `${shade}`);
-                    cell.style.backgroundColor = adjust(rgbToHex, cell.style.backgroundColor, shade * -2);
+                    cell.style.backgroundColor = adjust(rgbToHex, cell.style.backgroundColor, shade * -2);  // new value of color is calculated with current shade value
                 }
             }
         });    
     });
 }
 
+// if reset is clicked new grid is made, discarding the old grid and no other buttons is turned off as this button doesnt need to stay active
 let resetBtn = document.querySelector("#resetBtn");
 resetBtn.addEventListener('click', makeGrid);
 
+// returns the random value ranging from 0 <-> 255
 function getRandomColor()
 {
     return (Math.floor(Math.random() * 256));
 }
 
+// converts the RGB value into hex code which helps in manipulation of the shading value
 function rgbToHex(rgb) {
     let sep = rgb.indexOf(',') > -1 ? ',' : ' ';
 
@@ -184,6 +214,8 @@ function rgbToHex(rgb) {
     return '#' + r + g + b;
 }
 
+// new shading value is calculated based on the amount parameter
+// first the color is converted into hexCodes so to manipulate the code.
 function adjust(rgbToHex, rgb, amount) 
 {
     let color = rgbToHex(rgb);
